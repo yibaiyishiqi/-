5257f655507cc25304ae5dbec16cf965ae047f0d
yibaiyishiqi <3452136767@qq.com>
2023-11-08 18:01:34 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <img title="Vue logo" alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Vue</h1>
   </div>
 </template>
 
